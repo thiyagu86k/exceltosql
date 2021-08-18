dbf3be5ddbf886202cb4c799ae1f39f20020052d
thiyagu86k <thiyagu86@gmail.com>
2021-08-18 22:59:09 +0800

    1) Added the SQL Creation and Insertion 2) Still have the bug

--- src/main/java/com/fiablesystems/exceltosql/ExcelToSqlCommandLineRunner.java
+++ src/main/java/com/fiablesystems/exceltosql/ExcelToSqlCommandLineRunner.java
@@ -12,6 +12,9 @@ import org.springframework.util.ResourceUtils;
 
 import javax.sql.DataSource;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 @Component
@@ -23,6 +26,7 @@ public class ExcelToSqlCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         File file = ResourceUtils.getFile("classpath:TestXls.xlsx");
+        System.out.println(file.getAbsolutePath());
         ExcelReader excelReader=new ExcelReader();
 
        ExcelData excelData= excelReader.loadExcelFile(file);
@@ -34,18 +38,66 @@ public class ExcelToSqlCommandLineRunner implements CommandLineRunner {
             int len=keySet.size()-1;
             int index=0;
             for(String s: keySet){
-                createTableSQL.append(s).append(" STRING");
+                createTableSQL.append(s).append(" LONGVARCHAR");
                 if(index!=len){
                     createTableSQL.append(",");
                 }
                 index++;
             }
             createTableSQL.append(")");
+            jdbcTemplate.execute(createTableSQL.toString());
+            jdbcTemplate.execute("commit");
+            String[] strArray=keySet.toArray(new String[keySet.size()]);
+            List<Integer> eachColumnRowSize=new ArrayList<>();
+            for(String s:strArray){
+                eachColumnRowSize.add(e.getRowColumnData().get(s).size());
+            }
+            int highestLength= Collections.max(eachColumnRowSize);
+
+            int lenKeySet= keySet.size()-1;
+
+            List<String> stringList=new ArrayList<>();
+            StringBuffer stringBuffer=new StringBuffer();
+            StringBuffer valueBuffer=new StringBuffer();
+            int indx=0;
+            for(int i=0;i<highestLength;i++) {
+                int mapKey=0;
+
+                for (String s : strArray) {
+                    if (mapKey == 0) {
+                        stringBuffer.delete(0, stringBuffer.length());
+                        valueBuffer.delete(0, valueBuffer.length());
+                        stringBuffer.append("insert into ");
+                        stringBuffer.append(e.getSheetName());
+                        stringBuffer.append(" (");
+                        valueBuffer.append("values (");
+                    }
+                    stringBuffer.append(s+",");
+                    valueBuffer.append("\'"+e.getRowColumnData().get(s).get(indx)+"\',");
+                    if (mapKey == lenKeySet) {
+                        valueBuffer.deleteCharAt(valueBuffer.lastIndexOf(","));
+                        stringBuffer.deleteCharAt(stringBuffer.lastIndexOf(","));
+                        String insertStmt = valueBuffer.insert(0, stringBuffer.append(") ").toString()).append(")").toString();
+                        System.out.println(insertStmt);
+                        stringList.add(insertStmt);
+
+                        mapKey = 0;
+                    }
+
+                    mapKey++;
+                }
+                indx++;
+            }
+            for(String str:stringList){
+                jdbcTemplate.execute(str);
+            }
+            jdbcTemplate.execute("shutdown");
+
             //System.out.println(createTableSQL.toString());
          //   jdbcTemplate.execute(" CREATE TABLE ")
         });
 
-       // jdbcTemplate.execute(" CREATE TABLE t(a INTEGER, b BIGINT)");
+       // ;
       //  jdbcTemplate.execute("shutdown");
        // System.out.println("Table Created successfully");
     }
